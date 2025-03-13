5273ba11bd1f2626b177b39c15f84295aaa0193a
ethan <greymesa71@gmail.com>
2025-03-13 14:23:53 +0200

    Test demo

--- ShapeCalc/Program.cs
+++ ShapeCalc/Program.cs
@@ -1,3 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using ShapeCalc;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter circle radius");
+double radius = double.Parse(Console.ReadLine());
+
+Circle c = new Circle("steve", radius);
+
+c.Display();
+Console.WriteLine("Circle area: " + c.CalculateArea());
+
+Console.WriteLine("Enter rectangle width and height");
+double width = double.Parse(Console.ReadLine());
+double height = double.Parse(Console.ReadLine());
+
+Rectangle r = new Rectangle("bob", width, height);
+
+r.Display();
+Console.WriteLine("Rectangle area: " + r.CalculateArea());
